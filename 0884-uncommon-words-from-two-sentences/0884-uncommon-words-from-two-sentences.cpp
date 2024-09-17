5ff0e790a6bffec7f54df234a9b381a440ed263f
Akhil Tiwari <125031693+akhiltiwarietawah@users.noreply.github.com>
2024-09-17 19:09:42 +0530

    Time: 0 ms (100%), Space: 8.9 MB (72.35%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -2,36 +2,24 @@ class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         unordered_map<string, int> mpp;
-
         vector<string> ans;
+        
+        // Use stringstream to easily split both s1 and s2 by spaces
+        stringstream ss(s1 + " " + s2); // concatenate s1 and s2 with a space
+        string word;
 
-        string s = "";
-
-        for(auto ch : s1) {
-            if(ch == ' ') {
-                mpp[s]++;
-                s = "";
-            }
-            else s += ch;
+        // Parse words from the combined string and update frequency in the map
+        while (ss >> word) {
+            mpp[word]++;
         }
 
-        mpp[s]++;
-        s = "";
-
-        for(auto ch : s2) {
-            if(ch == ' ') {
-                mpp[s]++;
-                s = "";
-            }
-            else s += ch;
-        }
-        mpp[s]++;
-
-        for(auto it : mpp) {
-            if(it.second == 1) {
+        // Collect words that appear exactly once
+        for (const auto& it : mpp) {
+            if (it.second == 1) {
                 ans.push_back(it.first);
             }
         }
+
         return ans;
     }
 };

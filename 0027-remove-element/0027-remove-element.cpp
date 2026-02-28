3c5d98f0546ec819650979bbe99389fb9d2609a5
Akhil Tiwari <125031693+akhiltiwarietawah@users.noreply.github.com>
2026-02-28 19:31:37 +0530

    Time: 0 ms (100%), Space: 11.7 MB (83.04%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,15 +1,36 @@
+// best solution if order does NOT matter
+
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k=0;
-        for (int i=0; i<nums.size(); i++){
-            if(nums[i] != val){
-                nums[k] = nums[i];
-                k++;
+        int k = 0;
+        int n = nums.size();
+        while(k<n){
+            if(nums[k] == val){
+                nums[k] = nums[n-1];
+                n--;
             }
+            else k++;
         }
 
-        return k;
-        
+        return n;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         int k=0;
+//         for (int i=0; i<nums.size(); i++){
+//             if(nums[i] != val){
+//                 nums[k] = nums[i];
+//                 k++;
+//             }
+//         }
+
+//         return k;
+        
+//     }
+// };

57a749c7d57aa04bce8d199e25e844706c1006e4
Akhil Tiwari <125031693+akhiltiwarietawah@users.noreply.github.com>
2026-03-01 20:23:12 +0530

    Time: 0 ms (100%), Space: 30.6 MB (13.28%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,14 @@
+//first method with extra space O(n) nd tc == O(n)
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        vector<int> temp(n);
+        for(int i = 0; i < n; i++){
+            temp[(i+k)%n] = nums[i];
+        }
+        nums = temp;
+    }
+};

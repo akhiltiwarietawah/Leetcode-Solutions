eeff2101f825be68cd71113e565b8cff08bc5e91
Akhil Tiwari <125031693+akhiltiwarietawah@users.noreply.github.com>
2026-03-01 20:39:00 +0530

    Time: 0 ms (100%), Space: 29.7 MB (43.36%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,14 +1,40 @@
 //first method with extra space O(n) nd tc == O(n)
 
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         k %= n;
+//         vector<int> temp(n);
+//         for(int i = 0; i < n; i++){
+//             temp[(i+k)%n] = nums[i];
+//         }
+//         nums = temp;
+//     }
+// };
+
+
+
+
+// Method 2 using Cyclic replacement
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k %= n;
-        vector<int> temp(n);
-        for(int i = 0; i < n; i++){
-            temp[(i+k)%n] = nums[i];
+        int count = 0;
+        for(int start = 0; count < n; start++){
+            int current = start;
+            int prev = nums[start];
+
+            do{
+                int next = (current + k)%n;
+                int temp =  nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            }while(start != current);
         }
-        nums = temp;
     }
 };
